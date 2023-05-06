a5353de5a58262910be8ecc8029ebe52c1cd79c3
amir karimi <takfashomal@gmail.com>
2023-05-06 14:25:35 +0330

    update port s

--- pages/panel/edit_profile/index.vue
+++ pages/panel/edit_profile/index.vue
@@ -117,7 +117,7 @@
                   <label for="sport_club" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">باشگاه ورزشی</label>
                   <select id="sport_club"  v-model="user.profile_form.sport_club"  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <option value="">انتخاب کنید</option>
-                    <option v-for="item in user.sport_club" :value="item.id">{{ item.name }}</option>
+                    <option  :value="user.sport_club.id">{{ user.sport_club.name }}</option>
                   </select>
                 </div>
               </div>
